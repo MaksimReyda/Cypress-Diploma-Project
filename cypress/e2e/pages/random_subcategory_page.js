8b0dcc3d72b371fc7e83de08620d1cf6942da54b
MaksimReyda <reida.maksim@gmail.com>
2022-11-25 19:12:05 +0200

    Added new method: addToCartAllProducts

--- cypress/e2e/pages/random_subcategory_page.js
+++ cypress/e2e/pages/random_subcategory_page.js
@@ -1,9 +1,15 @@
 /// <reference types="cypress" />
 
+function getRandomNumber(min, max) {
+    let random = Math.random() * (max - min) + min
+    return Math.floor(random);
+}
+
 class randomSubcategoryPage {
 
     randomSubcategoryPageLocators = {
-        subcategories: '.subcategories > ul > li > a'
+        subcategories: '.subcategories > ul > li > a',
+        products: '.grid.list-inline.row.thumbnails > div > .thumbnail'
     }
 
     selectRabdomSubcategoty(){
@@ -24,7 +30,56 @@ class randomSubcategoryPage {
 
         }).then(function(){
             console.log(subcategories)
+            let randomNumber = getRandomNumber(0, subcategories.length)
+            cy.visit(subcategories[randomNumber].subcategoryLink)
+        })
+
+        return this
+    }
+
+    addToCartAllProducts(){
+
+        let productsData = []
+
+        cy.get(this.randomSubcategoryPageLocators.products).each(function($el, index, $list){
+            if( !$el.find('.pricetag').children().hasClass('nostock') ){
+
+                cy.getElementAttribute($el.find('.productcart'), 'href').then(function(link){
+                    if(link === '#'){
+                        console.log('CORRECT LINK')
+                        // cy.get(cy.wrap($el.find('.productcart'))).click()
+
+                        cy.get($el.prev().children()).then(function(productName){
+                    
+                            if($el.find('.price').children().hasClass('pricenew')){
+                                cy.get($el.find('.pricenew')).then(function(pricenew){
+                                    productsData.push({
+                                        name: productName.text().trim(),
+                                        price: pricenew.text().trim(),
+                                        quantity: '1'
+                                    })
+                                })
+                            } else{
+                                cy.get($el.find('.price').children()).then(function(price){
+                                    // prices.push(price.text().trim())
+                                    productsData.push({
+                                        name: productName.text().trim(),
+                                        price: price.text().trim(),
+                                        quantity: '1'
+                                    })
             
+                                })
+                            }
+        
+                        })
+        
+
+                        cy.wrap($el.find('.productcart')).click()
+                    }
+                })
+            }
+        }).then(function(){
+            console.log(productsData)
         })
 
         return this
--- cypress/e2e/tests/random_subcategory.cy.js
+++ cypress/e2e/tests/random_subcategory.cy.js
@@ -2,6 +2,12 @@
 
 import randomSubcategoryPage from '../pages/random_subcategory_page'
 import mainShopPage from '../pages/main_page'
+import shoppingCartPage from '../pages/shopping_chart_page'
+import checkoutConfirmationPage from '../pages/checkout_confirmaiton_page'
+import guestCheckoutPage from '../pages/guest_checkout_page'
+import {guestCheckoutPageData} from '../../fixtures/input_data'
+import orderDetailsPage from '../pages/order_details_page'
+
 
 describe('Random subcategory test', function(){
     it('Positive scenarion', function(){
@@ -11,6 +17,42 @@ describe('Random subcategory test', function(){
 
         randomSubcategoryPage
         .selectRabdomSubcategoty()
+        .addToCartAllProducts()
+
+
+        shoppingCartPage
+        .visitPage()
+        .clickCheckoutButton()
+
+        guestCheckoutPage
+        .selectGuestCheckoutRadioButton()
+        .clickContinueButton()
+        .checkOrderSummary()
+        .fillFirstNameInput(guestCheckoutPageData.firstName)
+        .fillLastNameInput(guestCheckoutPageData.lastName)
+        .fillEmailInput()
+        .fillAddress1Input(guestCheckoutPageData.address1)
+        .fillCityInput(guestCheckoutPageData.city)
+        .fillRegionSelect(3)
+        .fillZipCodeInput(guestCheckoutPageData.zipCode)
+        .clickContinueButton()
+
+
+        checkoutConfirmationPage
+        .checkBreadcrumbs()
+        .checkItemsInYourCart()
+        .clickConfirmOrderButton()
+        .openInvoicePage()
+        
+        orderDetailsPage
+        .openOrderDetailsPage()
+        // .checkFormValidation()
+        .fillOrderIdInput(true, true)
+        .fillEmailInput(true, true)
+        .clickContinueButton()
+        .checkFormValidation()
+        .checkOrderDetails()
+
 
     })
 })

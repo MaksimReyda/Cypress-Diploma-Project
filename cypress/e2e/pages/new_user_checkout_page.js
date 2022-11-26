c01ce88634e6a7b02887967ed73ab2d509d4b13b
MaksimReyda <reida.maksim@gmail.com>
2022-11-26 13:21:14 +0200

    Added fill in methods

--- cypress/e2e/pages/new_user_checkout_page.js
+++ cypress/e2e/pages/new_user_checkout_page.js
@@ -41,7 +41,114 @@ class newUserCheckoutPage {
 
         return this
     }
+
+    fillEmailInput(){
+        cy.generateEmail().then(function(generatedEmail){
+            cy.get('input#AccountFrm_email')
+                .click()
+                .clear()
+                .type(generatedEmail)
+        })
+
+        
+        return this
+    }
+
+    fillPhoneInput(phone){
+        cy.get(this.newUserCheckoutPageLocators.phoneNumber)
+            .click()
+            .clear()
+            .type(phone)
     
+        return this
+    }
+    
+    fillFaxInput(faxNumber){
+        cy.get(this.newUserCheckoutPageLocators.faxNumber)
+            .click()
+            .clear()
+            .type(faxNumber)
+
+        return this
+    }
+
+    fillCompanyInput(company){
+        cy.get(this.newUserCheckoutPageLocators.company)
+            .click()
+            .clear()
+            .type(company)
+
+        return this
+    }
+
+    fillAddress1Input(address1){
+        cy.get(this.newUserCheckoutPageLocators.address1)
+            .click()
+            .clear()
+            .type(address1)
+
+        return this
+    }
+
+    fillAddress2Input(address2){
+        cy.get(this.newUserCheckoutPageLocators.address2)
+            .click()
+            .clear()
+            .type(address2)
+
+        return this
+    }
+
+    fillCityInput(city){
+        cy.get(this.newUserCheckoutPageLocators.city)
+            .click()
+            .clear()
+            .type(city)
+
+        return this
+    }
+
+    fillRegionSelect(region){
+        cy.get(this.newUserCheckoutPageLocators.state)
+            .select(region)       
+        
+        return this
+    }
+
+    fillZipCodeInput(zipCode){
+        cy.get(this.newUserCheckoutPageLocators.zipCode)
+            .click()
+            .clear()
+            .type(zipCode)
+
+        return this
+    }
+
+    fillLoginNameInput(){
+
+        return this
+    }
+
+    fillPasswordInput(password){
+        cy.get(this.newUserCheckoutPageLocators.password)
+            .click()
+            .clear()
+            .type(password)
+
+        return this
+    }
+
+    fillConfirmPasswordInput(confirmPassword){
+        let today = new Date()
+        let time = today.getTime()
+        cy.get(this.newUserCheckoutPageLocators.confirmPassword)
+        .click()
+        .clear()
+        .type(`Maksym${time}`)
+
+        return this
+    }
+
 }
 
 module.exports = new newUserCheckoutPage()

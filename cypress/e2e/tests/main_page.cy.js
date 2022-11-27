00dc6d78c4514defe547a86c1fbbf9412a73c195
MaksimReyda <reida.maksim@gmail.com>
2022-11-27 22:26:12 +0200

    Added new testing scenarios for the main page

--- cypress/e2e/tests/main_page.cy.js
+++ cypress/e2e/tests/main_page.cy.js
@@ -6,43 +6,83 @@ import {mainPageData} from '../../fixtures/input_data'
 
 
 describe('The Main page test', function(){
+
+    this.beforeEach(function(){
+        mainPage.visitMainPage()
+    })
+
     let result
-    it('Positive scenario', function(){
+    it('Positive scenario: Check main page buner', function(){
         // mainPage.visitMainPage()
         //     .getSlideText()
-        mainPage.visitMainPage()
+        mainPage
             .checkSlide(mainPage.mainShopPageLocators.bunner.slide1, mainPageData.banner.slide1)
             .moveSlide()
             .checkSlide(mainPage.mainShopPageLocators.bunner.slide2, mainPageData.banner.slide2)
             .moveSlide()
             .checkSlide(mainPage.mainShopPageLocators.bunner.slide3, mainPageData.banner.slide3)
-            .checkProductTitle(mainPage.mainShopPageLocators.featuredProducts[0].title, mainPageData.featuredProducts[0].name)
-            .checkIsProductOnStock(mainPage.mainShopPageLocators.featuredProducts[0].chartBtn, true)
-            .checkProductTitle(mainPage.mainShopPageLocators.featuredProducts[0].price, mainPageData.featuredProducts[0].price)
+            // .checkProductTitle(mainPage.mainShopPageLocators.featuredProducts[0].title, mainPageData.featuredProducts[0].name)
+            // .checkIsProductOnStock(mainPage.mainShopPageLocators.featuredProducts[0].chartBtn, true)
+            // .checkProductTitle(mainPage.mainShopPageLocators.featuredProducts[0].price, mainPageData.featuredProducts[0].price)
     })
 
-    it.only('', function(){
+
+    it('Positive scenario: Check categories names', function(){
+        mainPage
+            .checkCategories()
+    })
+
+
+    it('Positive scenario: Check subcategories names', function(){
+        mainPage
+            .checkSubcategories()
+    })
+
+    it('Positive scenario: Check out of stock products', function(){
+        mainPage
+            .checkOutOfStockProducts()
+    })
+
+    it('Positive scenario: Check price with default currency', function(){
+        mainPage
+            .checkPrice(mainPageData.featuredProducts)
+            .checkPrice(mainPageData.latestProducts)
+            .checkPrice(mainPageData.bestsellersProducts)
+            .checkPrice(mainPageData.specialsProducts)
+    })
+
+    it('Positive scenario: Change currency and check the price', function(){
+        mainPage
+            .checkPrice(mainPageData.featuredProducts)
+            .checkPrice(mainPageData.latestProducts)
+            .checkPrice(mainPageData.bestsellersProducts)
+            .checkPrice(mainPageData.specialsProducts)
+            .changeCurrency('Euro')
+            .checkPrice(mainPageData.featuredProducts)
+            .checkPrice(mainPageData.latestProducts)
+            .checkPrice(mainPageData.bestsellersProducts)
+            .checkPrice(mainPageData.specialsProducts)
+            .changeCurrency('Sterling')
+            .checkPrice(mainPageData.featuredProducts)
+            .checkPrice(mainPageData.latestProducts)
+            .checkPrice(mainPageData.bestsellersProducts)
+            .checkPrice(mainPageData.specialsProducts)
+            .changeCurrency('Dollar')
+            .checkPrice(mainPageData.featuredProducts)
+            .checkPrice(mainPageData.latestProducts)
+            .checkPrice(mainPageData.bestsellersProducts)
+            .checkPrice(mainPageData.specialsProducts)
+    })
+
+    it('Scenario: Add rundom product to cart', function(){
+        mainPage
+            .addProductToChart()
+    })
+
+    it('Scenario: Combine all tests together', function(){
         let section = 'Featured'
-        // cy.request('GET', 'https://automationteststore.com/').then(function(response){
-        //     // console.log(response.body.Items)
-        //     // result = response.body.Items
-        //     // console.log(response)
-        //     result = HTMLParser.parse(response.body)
-        //     // console.log(result)
 
-        //     function recursy(element){
-        //         element.childNodes.forEach(node => {
-        //             // console.log(node)
-        //             if(element.childNodes.lenght > 1){
-        //                 recursy(node)
-        //             }
-        //         });
-        //     }
-
-        //     recursy(result)
-
-        // })
-        mainPage.visitMainPage()
+        mainPage
             .checkCategories()
             .checkSubcategories()
             .checkPromoSection()

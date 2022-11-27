e49162d981115ef3e8b5dcc4db2eadf90d2f644d
MaksimReyda <reida.maksim@gmail.com>
2022-11-27 23:07:41 +0200

    Added new tests for the Shopping cart page

--- cypress/e2e/tests/shopping_chart.cy.js
+++ cypress/e2e/tests/shopping_chart.cy.js
@@ -5,18 +5,69 @@ import shoppingCartPage from '../pages/shopping_chart_page'
 
 
 describe('Testing the Shopping chart page', function(){
-    it("Positive scenario", function(){
+
+    it('Positive scenario: Check the cart table', function(){
+        
         cy.AddProductToCart(0)
         cy.AddProductToCart(5)
 
         shoppingCartPage
-        .visitPage()
-        .checkCartTable()
-        .removeItemFromCart()
-        .clickCheckoutButton()
+            .visitPage()
+            .checkCartTable()
+    })
+
+    it('Positive scenario: Add one product to cart and remove it', function(){
+
+        cy.AddProductToCart(0)
+
+        shoppingCartPage
+            .visitPage()
+            .checkCartTable()
+            .removeItemFromCart()
+            .checkCartTable()
+    })
+
+    it('Positive scenario: Add two products to cart and remove one', function(){
+
+        cy.AddProductToCart(0)
+        cy.AddProductToCart(5)
+
+        shoppingCartPage
+            .visitPage()
+            .checkCartTable()
+            .removeItemFromCart()
+            .checkCartTable()
+    })
+
+    it('Positive scenario: Add two products to cart and remove them', function(){
+
+        cy.AddProductToCart(0)
+        cy.AddProductToCart(5)
+
+        shoppingCartPage
+            .visitPage()
+            .checkCartTable()
+            .removeItemFromCart()
+            .checkCartTable()
+            .removeItemFromCart()
+            .checkCartTable()
+    })
+
+    it("Positive scenario: Add two products to cart, remove one and click the Continue button", function(){
+
+        cy.AddProductToCart(0)
+        cy.AddProductToCart(5)
+
+        shoppingCartPage
+            .visitPage()
+            .checkCartTable()
+            .removeItemFromCart()
+            .clickCheckoutButton()
         // .viewCart()
         // .removeItemFromCart()
 
         // isTopCartEmpty()
     })
+
+
 })
